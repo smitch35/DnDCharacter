8daa4cf96ce7406819ca97c135e654227e5500f1
Sean Mitchuson <sean.mitchuson@gmail.com>
2026-03-06 09:01:13 -0500

    working to combine character info into single file

--- character_build.py
+++ character_build.py
@@ -23,5 +23,17 @@ def main():
     input("Up next are your character stats.  This is the fun part! Press Enter to continue!")
 
     character_stats.dice_rolls()
+    character_sheet = open('charactersheeet.txt', 'w')
+    data_1 = open('charactername.txt')
+    data_1_import = data_1.read()
+    data_2 = open('class.txt')
+    data_2_import = data_2.read()
+    data_3 = open('origin.txt')
+    data_3_import = data_3.read()
+    data_4= open('stats.txt')
+    data_4_import = data_4.read()
+    data_print = str(data_1 + data_2 + data_3 + data_4)
+    character_sheet.write(data_print)
+    print("You have finished your character creation.  You will now have a charactersheet.txt file to reference all your information!")
 
 main()
